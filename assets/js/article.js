7550d4adb52b9959ab7ec01203045832b80f0aa1
theajack <1506282385@qq.com>
2017-03-07 16:45:57 +0800

    17-3-7

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,12 +1,45 @@
 var isMobile;
 var c_name=["d-show","d-hide","section","code","wechat-img","part","head-right","comment","face-box","face-item"];
 var i_name=["footer","footerLink","title"];
+var c_data=[
+  {
+    "name":"theajack",
+    "photo":"assets/images/bikeshare.png",
+    "date":"2017-09-09",
+    "content":"大撒反对仿盛大首发式大苏打撒",
+  },{
+    "name":"theajack",
+    "photo":"assets/images/bikeshare.png",
+    "time":"2017-09-09",
+    "content":"大撒反对仿盛大首发式大苏打撒",
+  },{
+    "name":"theajack",
+    "photo":"assets/images/bikeshare.png",
+    "time":"2017-09-09",
+    "content":"大撒反对仿盛大首发式大苏打撒",
+  },{
+    "name":"theajack",
+    "photo":"assets/images/bikeshare.png",
+    "time":"2017-09-09",
+    "content":"大撒反对仿盛大首发式大苏打撒aaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  },{
+    "name":"theajack",
+    "photo":"assets/images/bikeshare.png",
+    "time":"2017-09-09",
+    "content":"大撒反对仿盛大首发式大苏打撒aaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  }
+];
 J.ready(function(){
   init();
   initClass();
   setStyle();
+  bindComment();
 })
+function bindComment(){
+  
+}
 function init(){
+  Jet.lang("chinese");
   initFaceBox();
   J.class("wechat-public").event({
     "onmouseover":"J.class('wechat-img').fadeIn()",
@@ -26,6 +59,21 @@ function initFaceBox(){
       this.attr("data-show","true");
       this.next().fadeIn(null,'fast');
     }
+  });
+  J.class("c-link").event("onclick",function(){
+    var obj=this;
+    Jet.input({
+      title:"添加链接",
+      text:["链接地址","链接文字"],
+      default:["http://",null],
+      valid:["url",null],
+      placeholder:["请填写正确的url地址","默认值为链接地址"],
+  　},function(data){
+      if(data[1]==""){
+        data[1]=data[0];
+      }
+      J.id(obj.attr("data-aim")).append(J.new("span.span-link[contenteditable=false][onclick=Jet.jump('"+data[0]+"')]").text(data[1]));
+    });
   });
   for(var i=1;i<=40;i++){
     box.append(J.new("img.face-item[src=assets/images/rabbit/rabbit ("+i+").gif][onclick=addFace(this)]"));
